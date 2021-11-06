8e2eb2f9ad44bb79478e3731b47f60f4601d8242
David Hoefs <hoefsdavid9701@gmail.com>
2021-11-05 19:47:26 -0500

    initial commit

---
+++ Deconstructor/Deconstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+// Tuple
+var person = (FirstName: "David", LastName: "Hoefs");
+// Deconstruct Tuple
+//(string firstName, string lastName) = person;
+
+
+
+//// Another way to deconstuct
+//string fn;
+//string ln;
+//(fn, ln) = person;
+
+
+string firstName;
+(firstName, string lastName) = person;
+Console.WriteLine(firstName);
+Console.WriteLine(lastName);
---
+++ NullParameterChecks/NullParameterChecks/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string? info = null;
+
+SayHi(info);
+
+static void SayHi(string message)
+{
+
+    // No more if statement!!
+    ArgumentNullException.ThrowIfNull(message);
+
+    Console.WriteLine($"Hello {message}");
+}
