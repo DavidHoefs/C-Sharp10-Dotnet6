709f842e3c93717832a5faa43e948b401efc68b9
David Hoefs <hoefsdavid9701@gmail.com>
2021-11-05 20:03:24 -0500

    Initial Commit

---
+++ ExtendedPropertyPatterns/ExtendedPropertyPatterns/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+
+
+using System.Diagnostics;
+
+PersonModel p = new();
+/// <summary>
+/// Old way of doing it
+/// </summary>
+if (p is { FirstName: "David" , HomeAddress:{ City:"Lubbock"} })
+{
+    Console.WriteLine("Hello David");
+}
+
+/// <summary>
+/// New way of doing it 
+/// </summary>
+if(p is { FirstName: "David", HomeAddress.City: "Lubbock" })
+{
+    
+    Console.WriteLine("Hello David");
+}
+
+
+
+
+public class PersonModel
+{
+    
+    public string FirstName { get; set; } = "David";
+
+    public string LastName { get; set; } = "Hoefs";
+
+    public AddressModel HomeAddress { get; set; } = new AddressModel();
+
+
+}
+
+
+public class AddressModel
+{
+
+    public string City { get; set; } = "Lubbock";
+    public string State { get; set; } = "Texas";
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
+
