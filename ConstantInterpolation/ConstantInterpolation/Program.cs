f30c06f76efc2c255f953768abe88e8cbcdba69f
David Hoefs <hoefsdavid9701@gmail.com>
2021-11-06 14:44:22 -0500

    Constant Interpolation Added

---
+++ ConstantInterpolation/ConstantInterpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿string myCompany = "David's Company";
+const string companyName = "Acme";
+const string productName = $"{companyName} Anvils";
+const string productDescription = $"{productName} are the best way to crush unsuspecting roadrunners";
+
+Console.WriteLine(productDescription);
+
+[Obsolete($"This is no longer used for {productName}")]
+void SayHi()
+{
+    
+}
